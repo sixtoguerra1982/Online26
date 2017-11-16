805974bea378c3a47c3cbf6bb9664bf37472ad4b
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2017-11-16 08:07:47 -0300

    botones

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'events/index'
+  resources :events
+  get 'pages/index'
+  root to: 'pages#index'
 
-  get 'events/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
